f90a23c4bd0573c07c3e74e147886805ae47eede
Bei Lin <129919638+lamberlin@users.noreply.github.com>
2023-10-26 00:18:11 -0700

    Update map.py

--- map.py
+++ map.py
@@ -13,20 +13,20 @@ colormap = branca.colormap.linear.RdBu_11.scale(min_k, max_k)
 topo_tile_url = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
 m = folium.Map(location=[15.7835, -90.2308], zoom_start=6, tiles=topo_tile_url, attr='Esri')
 
-plot_dir = "./segments_site_plots/"
+github_base_url = "https://raw.githubusercontent.com/lamberlin/map-of-k/main/segments_site_plots/"
 
 for _, row in site_data.iterrows():
     color = colormap(row['k1'])
     
-    # Construct the path to the plot image for the site
-    plot_path = plot_dir + f"growth_curve_{row['codigoparc']}.png"
+    # Construct the URL to the plot image for the site on GitHub
+    plot_url = github_base_url + f"growth_curve_{row['codigoparc']}.png"
     
     # Create a string combining the popup content and the image
     popup_content = f"""
     Site: {row['codigoparc']}<br>
     Species: {row['Especie']}<br>
     k: {row['k1']}<br>
-    <img src="{plot_path}" alt="site_plot" width="300">
+    <img src="{plot_url}" alt="site_plot" width="300">
     """
     
     folium.CircleMarker(
@@ -42,8 +42,6 @@ for _, row in site_data.iterrows():
 colormap.caption = "k Value"
 colormap.add_to(m)
 
-folium_static(m)
-
 st.title("Growth Curve Map of Plantation Across Guatemala")
 st.subheader("Created by Lambert Lin, Ja Yoon Kim")
-
+folium_static(m)
