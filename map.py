a2ced30ed13d8a4d08044250ea078f85afdc6ac4
Bei Lin <129919638+lamberlin@users.noreply.github.com>
2023-10-25 23:00:46 -0700

    Update map.py

--- map.py
+++ map.py
@@ -1,39 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-# streamlit_app.py
-
-import streamlit as st
 import pandas as pd
-import pydeck as pdk
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 
-# Load data
-site_data = pd.read_csv("./segments_site_k_values.csv")
+site_data = pd.read_csv(r"./segments_site_k_values.csv")
 
-# Create map with pydeck
-view_state = pdk.ViewState(latitude=15.7835, longitude=-90.2308, zoom=5)
-layer = pdk.Layer(
-    "ScatterplotLayer",
-    data=site_data,
-    get_position=["X", "Y"],
-    get_color="k1 * 255",
-    get_radius=50000,
-    pickable=True,
-    radius_min_pixels=10,
-    radius_max_pixels=50
-)
+fig, ax = plt.subplots(subplot_kw={'projection': ccrs.PlateCarree()}, figsize=(10, 10))
 
-tooltip = {
-    "html": "<b>Site:</b> {codigoparc}<br><b>Species:</b> {Especie}<br><b>k:</b> {k1}",
-    "style": {"backgroundColor": "steelblue", "color": "white"}
-}
+# Add Stamen terrain background
+ax.add_image(cfeature.STAMEN_TERRAIN, 6)  # '6' is the zoom level; you can adjust it as needed
 
-r = pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip=tooltip)
+# Plot sites with k values
+sc = ax.scatter(site_data['X'], site_data['Y'], c=site_data['k1'], 
+                s=50, edgecolor='k', cmap='RdBu', transform=ccrs.PlateCarree())
 
-# Streamlit app
-st.title('Distribution of k values in Sites across Guatemala')
-st.pydeck_chart(r)
+# Add colorbar
+cbar = plt.colorbar(sc, ax=ax, orientation='horizontal', fraction=0.046, pad=0.1)
+cbar.set_label('k Value')
 
+plt.title('Distribution of k values in Sites across Guatemala')
+plt.show()
