a13e08784a3f142f41f8a14dca1597fbc0a3d368
Bei Lin <129919638+lamberlin@users.noreply.github.com>
2023-11-01 11:35:22 -0700

    Update map.py

--- map.py
+++ map.py
@@ -16,14 +16,14 @@ st.markdown(
         }}
 
         .map-frame {{
-            border: 25px solid #8B4513;  /* Increased border width and changed color to a brown (like wood) */
-            border-radius: 15px;       /* Adjusted border radius for a more rounded appearance */
-            overflow: hidden;          /* Hide any content overflowing the frame */
-            box-shadow: 5px 5px 15px rgba(0,0,0,0.4); /* Add shadow for depth */
+            border: 25px solid #8B4513;
+            border-radius: 15px;
+            overflow: hidden;
+            box-shadow: 5px 5px 15px rgba(0,0,0,0.4);
         }}
         
         .leaflet-container {{
-            box-shadow: 0 0 20px 20px white;  /* Adjust as needed */
+            box-shadow: 0 0 20px 20px white;
         }}
     </style>
     """,
@@ -41,33 +41,36 @@ m = folium.Map(location=[15.7835, -90.2308], zoom_start=6, tiles=topo_tile_url, 
 
 github_base_url = "https://raw.githubusercontent.com/lamberlin/map-of-k/main/segments_site_plots/"
 
-for _, row in site_data.iterrows():
-    color = colormap(row['k1'])
-    
-    # Construct the URL to the plot image for the site on GitHub
-    plot_url = github_base_url + f"growth_curve_{row['codigoparc']}.png"
-    
-    # Create a string combining the popup content and the image
-    popup_content = f"""
-    Site: {row['codigoparc']}<br>
-    Species: {row['Especie']}<br>
-    k: {row['k1']}<br>
-    <img src="{plot_url}" alt="site_plot" width="300">
-    """
-    
-    folium.CircleMarker(
-        location=[row['Y'], row['X']],
-        radius=5,
-        color=color,
-        fill=True,
-        fill_color=color,
-        fill_opacity=0.6,
-        popup=folium.Popup(popup_content, max_width=310) 
-    ).add_to(m)
-
-colormap.caption = "k Value"
-colormap.add_to(m)
-
 st.title("Growth Curve Map of Plantation Across Guatemala")
 st.subheader("Created by Lambert Lin, Ja Yoon Kim")
-folium_static(m)
+
+# User selection for k value just below the subheader and above the map
+selected_k = st.selectbox(
+    "Select k value for visualization:",
+    ("k1", "k2", "k3", "k4")
+)
+
+for _, row in site_data.iterrows():
+    if pd.notna(row[selected_k]):
+        color = colormap(row[selected_k])
+        plot_url = github_base_url + f"growth_curve_{row['codigoparc']}.png"
+        popup_content = f"""
+        Site: {row['codigoparc']}<br>
+        Species: {row['Especie']}<br>
+        {selected_k}: {row[selected_k]}<br>
+        <img src="{plot_url}" alt="site_plot" width="300">
+        """
+        folium.CircleMarker(
+            location=[row['Y'], row['X']],
+            radius=5,
+            color=color,
+            fill=True,
+            fill_color=color,
+            fill_opacity=0.6,
+            popup=folium.Popup(popup_content, max_width=310) 
+        ).add_to(m)
+
+colormap.caption = f"{selected_k} Value"
+colormap.add_to(m)
+
+st.write(folium_static(m))
